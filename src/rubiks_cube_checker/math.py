a2edfbf60beb929dc7fbe00c572c631b03de621e
naweiss <naweiss@g.jct.ac.il>
2023-05-20 22:51:01 +0300

    Simplify permutation_parity implementation

--- src/rubiks_cube_checker/math.py
+++ src/rubiks_cube_checker/math.py
@@ -13,21 +13,17 @@ def permutation_parity(current_state: List[Tuple[str, ...]], solved_state: List[
         element: solved_element
         for element, solved_element in zip(current_state, solved_state)
     }
+
     parings = _sort_dict(parings)
     keys = list(parings.keys())
-    start_key = keys.pop()
-    key = start_key
     permutations = 0
     while len(keys) > 0:
-        value = parings[key]
-        if value == start_key:
-            start_key = keys.pop()
-            key = start_key
-        else:
+        start_key = keys.pop()
+        key = start_key
+        while True:
+            key = parings[key]
+            if key == start_key:
+                break
             permutations += 1
-            key = value
             keys.remove(key)
-    value = parings[key]
-    if value != start_key:
-        permutations += 1
     return permutations % 2
